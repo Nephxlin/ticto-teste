6cd9123d05fc21ff47044bd1a7a3a914fe13e4a8
Luther Marques Cordeiro <luther.m.cordeiro@gmail.com>
2023-04-13 20:43:27 -0300

    Initial code base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
+import { Roboto, Poppins } from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['300', '500', '700'], variable: '--font-Roboto' })
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '500', '700'], variable: '--font-Poppins' })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={`${roboto.className, poppins.className}`}>
+        <Component {...pageProps} className={`${roboto.variable} ${poppins.variable}`} />
+      </main>
+    </>
+  )
 }
