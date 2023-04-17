113f3ae7d57142549d58b2b27ecc01c9042e9aa6
Luther Marques Cordeiro <luther.m.cordeiro@gmail.com>
2023-04-17 17:12:21 -0300

    fix: next scss config bundle

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 const path = require('path')
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'src')],
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
